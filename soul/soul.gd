5db61c7344d274d13852adebf9bccaa28f647d3a
jouluhiisi <uye27971@gmail.com>
2025-07-23 00:43:47 -0300

    Add files via upload

--- soul/soul.gd
+++ soul/soul.gd
@@ -9,7 +9,7 @@ var active := false:
 		grazed_pellets.clear()
 var grazed_pellets: Array[Pellet] = []
 var graze_timer := 0.0
-var invulnerable := true
+var invulnerable := false
 
 
 func _physics_process(delta: float) -> void:
@@ -30,7 +30,6 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_pressed("cancel"):
 			velocity *= 0.5
 	move_and_slide()
-	modulate
 	for pellet: Pellet in grazed_pellets:
 		graze(pellet, delta)
 
@@ -38,12 +37,12 @@ func hurt(p_damage: int) -> void:
 	if(invulnerable):
 		return
 	get_parent().hurt(5 * p_damage)
+	invulnerable = true
+	$AnimationPlayer.play("invulnerable")
 		
 	
 
 func _on_tp_range_area_entered(p_area: Area2D) -> void:
-	if(invulnerable):
-		return
 	grazed_pellets.append(p_area)
 	graze(p_area, 1.0 / Engine.max_fps)
 
@@ -51,6 +50,8 @@ func _on_tp_range_area_exited(p_area: Area2D) -> void:
 	grazed_pellets.erase(p_area)
 
 func graze(p_pellet: Pellet, p_delta: float) -> void:
+	if(invulnerable):
+		return
 	if p_pellet.grazed:
 		Global.tp += 30.0 * p_delta * p_pellet.graze_points / 20.0
 		if get_parent().turn_timer >= 1.0 / 3.0:
@@ -68,6 +69,7 @@ func graze(p_pellet: Pellet, p_delta: float) -> void:
 		graze_timer = 1.0 / 3.0
 
 
-func _on_animation_player_animation_finished(invulnerable):
-	invulnerable = false
+func _on_animation_player_animation_finished(anim_invulnerable):
+	if anim_invulnerable == "invulnerable":
+		invulnerable = false
 	pass # Replace with function body.
