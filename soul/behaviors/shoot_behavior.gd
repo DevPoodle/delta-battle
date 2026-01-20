1237f694725c5871f48e8f4ae603b39f6ad676dd
DP <digitalpeargaming@gmail.com>
2026-01-19 22:03:26 -0800

    yellow soul animations
    Added rudimentary animations so big shots are telegraphed.
    The animation is not very accurate, but it should function correctly.

--- soul/behaviors/shoot_behavior.gd
+++ soul/behaviors/shoot_behavior.gd
@@ -13,6 +13,8 @@ var current_cooldown : float = 0
 @export var big_shot_holding_duration : float = 1.0
 var holdingDuration : float = 0
 
+@export var bullets : Array[JusticePellet] = []
+
 func start() -> void:
 	set_direction(face_direction)
 
@@ -21,21 +23,26 @@ func tick(delta : float) -> void:
 		if holdingDuration > 0:
 			holdingDuration = 0
 		return
+	current_cooldown = maxf(0, current_cooldown-delta)
 	if Input.is_action_pressed("confirm"):
 		holdingDuration += delta
-	current_cooldown-=delta
-	if holdingDuration > 0 && !Input.is_action_pressed("confirm") && current_cooldown <= 0:
-		shoot(holdingDuration>big_shot_holding_duration)
-		holdingDuration = 0
-		current_cooldown = shooting_cooldown
-	if holdingDuration >= big_shot_holding_duration:
-		animateBigShottedness()
+		if holdingDuration >= big_shot_holding_duration:
+			animateBigShottedness(delta)
+	else:
+		if holdingDuration > 0 && current_cooldown <= 0:
+			shoot(holdingDuration>big_shot_holding_duration)
+			holdingDuration = 0
+			current_cooldown = shooting_cooldown
+			soul.heart.modulate = soul.get_base_color()
 
+func turn_end() -> void:
+	for bullet in bullets.filter(func(bul): return bul != null):
+		bullet.queue_free()
+	bullets.clear()
 
-##Doesn't work. IDK why. It probably hates me.
-func animateBigShottedness() -> void:
-	var amount : float = absf(sin(Time.get_ticks_msec())) * 2
-	soul.modulate = soul.get_base_color().lerp(Color.WHITE_SMOKE, amount)
+func animateBigShottedness(delta : float) -> void:
+	var amount : float = absf(sin(Time.get_ticks_msec()))
+	soul.heart.modulate = soul.get_base_color().lerp(Color.WHITE_SMOKE, amount)
 
 func shoot(is_big_shot : bool = false) -> void:
 	var newPellet : JusticePellet = JUSTICE_PELLET.instantiate()
@@ -43,6 +50,7 @@ func shoot(is_big_shot : bool = false) -> void:
 	newPellet.z_index = soul.z_index
 	newPellet.is_big_shot = is_big_shot
 	newPellet.global_position = soul.global_position + Vector2(8, 0).rotated(deg_to_rad(degrees + 90)) * soul.scale.x
+	bullets.append(newPellet)
 	soul.get_parent().add_child(newPellet)
 
 func set_direction(dir : Util.Direction) -> void:
